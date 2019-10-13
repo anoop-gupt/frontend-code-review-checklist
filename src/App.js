ff7f34d30d2ff760507aba938f740c0d5a4653cb
mayank.singh <mayank.singh@publicissapient.com>
2019-10-13 16:53:09 +0100

    refactoring the code for common reusable code

--- src/App.js
+++ src/App.js
@@ -7,105 +7,40 @@ function App() {
   return (
     <div className="App container">
       <header className="App-header">Code review CheckList</header>
-      <div className="content">
-      <Collapsible trigger="Code Styling">
-          {checkList["code-style"].map((data, index) => (
-            <div className="form-check" key={index}>
-              <input
-                className="form-check-input"
-                type="checkbox"
-                value=""
-                id="defaultCheck1"
-              />
-              <label className="form-check-label" for="defaultCheck1">
-                {data}
-              </label>
-            </div>
-          ))}
-        </Collapsible>
-        <Collapsible trigger="Code formating">
-          {checkList["code-formating"].map((data, index) => (
-            <div className="form-check" key={index}>
-              <input
-                className="form-check-input"
-                type="checkbox"
-                value=""
-                id="defaultCheck1"
-              />
-              <label className="form-check-label" for="defaultCheck1">
-                {data}
-              </label>
-            </div>
-          ))}
-        </Collapsible>
-        <Collapsible trigger="Architecture (Separation of Concerns)">
-          {checkList["architecture"].map((data, index) => (
-            <div className="form-check" key={index}>
-              <input
-                className="form-check-input"
-                type="checkbox"
-                value=""
-                id="defaultCheck1"
-              />
-              <label className="form-check-label" for="defaultCheck1">
-                {data}
-              </label>
-            </div>
-          ))}
-        </Collapsible>
-        <Collapsible trigger="Coding best practices">
-          {checkList["coding-best-practices"].map((data, index) => (
-            <div className="form-check" key={index}>
-              <input
-                className="form-check-input"
-                type="checkbox"
-                value=""
-                id="defaultCheck1"
-              />
-              <label className="form-check-label" for="defaultCheck1">
-                {data}
-              </label>
-            </div>
-          ))}
-        </Collapsible>
-        <Collapsible trigger="Non funcatinal">
-          {checkList["non-funcatinal"].map((data, index) => (
-            <Collapsible trigger={Object.keys(data)[0]}>
-              {data[Object.keys(data)[0]].map((innerData, index) => (
-                <div className="form-check" key={`inner` + index}>
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    value=""
-                    id="defaultCheck1"
-                  />
-                  <label className="form-check-label" for="defaultCheck1">
-                    {innerData}
-                  </label>
-                </div>
-              ))}
-            </Collapsible>
-          ))}
-        </Collapsible>
-        <Collapsible trigger="Tools for review">
-          {checkList["tools"].map((data, index) => (
-            <div className="form-check" key={index}>
-              <input
-                className="form-check-input"
-                type="checkbox"
-                value=""
-                id="defaultCheck1"
-              />
-              <label className="form-check-label" for="defaultCheck1">
-                {data}
-              </label>
-            </div>
-          ))}
-        </Collapsible>
-      </div>
-      <footer></footer>
+      <div className="content">{Accordion(checkList)}</div>
     </div>
   );
+}
+
+function Accordion(category, header) {
+  if (Array.isArray(category)) {
+    return (
+      <Collapsible trigger={header} key={header}>
+        {category.map((data, index) => (
+          <div className="form-check" key={header + index}>
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="defaultCheck1"
+            />
+            <label className="form-check-label" htmlFor="defaultCheck1">
+              {data}
+            </label>
+          </div>
+        ))}
+      </Collapsible>
+    );
+  } else {
+    const categoryKeys = Object.keys(category);
+    return header ? (
+      <Collapsible trigger={header} key={header}>
+        {categoryKeys.map(keys => Accordion(category[keys], keys))}
+      </Collapsible>
+    ) : (
+      categoryKeys.map(keys => Accordion(category[keys], keys))
+    );
+  }
 }
 
 export default App;
